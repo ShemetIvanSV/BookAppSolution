94285318f66ddb785aa1924fe27137682ed4ab7c
Иван Шемет <schemet2014@gmail.com>
2022-10-10 09:03:09 +0300

    Добавьте файлы проекта.

---
+++ BookServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// связанные со сборкой.
+[assembly: AssemblyTitle("BookServices")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookServices")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Если для атрибута ComVisible задать значение FALSE, типы в этой сборке будут невидимыми 
+// для компонентов COM. Если в этой сборке необходимо получить доступ к типу из 
+// компонента COM, задайте для атрибута ComVisible этого типа значение TRUE.
+[assembly: ComVisible(false)]
+
+// Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+[assembly: Guid("63f95668-1986-4ae9-96f0-4e8394569647")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно указать все значения или установить для номеров редакции и сборки значение по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
